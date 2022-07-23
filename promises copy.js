5ef7a268f6b9a4b54b9d4140aefcf1d7c61316ee
eddiesmag <eddiesmag@gmail.com>
2022-07-23 15:07:43 +0300

    add promises

---
+++ promises copy.js
@@ -0,0 +1,62 @@
+/** Basic Promises */
+/*var promise = new Promise(function(resolve, reject) {
+    var myBoy = false;
+
+    if(!myBoy) {
+        resolve("Stuff worked....");
+    } else {
+        reject(Error("Stuff Broke!!!!"));
+    }
+})
+
+// promise.then(function(result) {
+//     console.log(result);
+// }, function(err) {
+//     console.log(err);
+// })
+
+promise.then(function(result) {
+    console.log(result);
+}).catch(function(err) {
+    console.log(err)
+})*/
+
+/** Promises Example */
+
+var functions = function() {
+    return new Promise(function(resolve, reject) {
+        resolve("Learnt JS functions,");
+    });
+}
+
+var callBacks = function(message) {
+    return new Promise(function(resolve, reject) {
+        resolve(message + " learnt Callback functions &");
+    })
+}
+
+var promises = function(message) {
+    return new Promise(function(resolve, reject) {
+        resolve(message + " Good to go for JavaScript Promises!");
+    })
+}
+
+functions().then(function(result) {
+    return callBacks(result);
+}).then(function(result) {
+    return promises(result);
+}).then(function(result){
+    // console.log("Good to go : " + result);
+}).catch(function(err) {
+    return err;
+})
+ let id = 0;
+const msg = () => alert("i ran after 3sec!!!!");
+
+function Start() {
+    id = setInterval(msg, 5000);
+}
+
+function Stop() {
+    clearTimeout(id);
+}
